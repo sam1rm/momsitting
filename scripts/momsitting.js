18c48e847011b05f16b8f57ddb699ab7ad0038b6
Elizabeth Lin <elizabethylin@gmail.com>
2014-01-25 16:57:31 -0800

    Adding CSS

--- scripts/momsitting.js
+++ scripts/momsitting.js
@@ -3,7 +3,22 @@ $(document).ready(function() {
     $(".module-background, .module-unit").addClass("module-active");
   });
   $(".module-background").click(function(){
-    console.log("here");
     $(".module-background, .module-unit").removeClass("module-active");
   });
+  $(".menu-trigger").click(function(){
+    $(".general-nav").toggleClass("active");
+  });
+
+  $( "#slider-range" ).slider({
+    range: true,
+    min: 0,
+    max: 500,
+    values: [ 75, 300 ],
+    slide: function( event, ui ) {
+      $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
+    }
+  });
+  $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
+    " - $" + $( "#slider-range" ).slider( "values", 1 ) );
+
 });
