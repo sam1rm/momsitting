1a77f13e4942b89aadb7669261323ba2d263d681
Samir Makhani <makhani@berkeley.edu>
2014-01-27 10:12:26 -0800

    flask integration + heroku ready

--- app/views.py
+++ app/views.py
@@ -5,6 +5,26 @@ from flask import render_template
 def index():
 	return render_template('index.html')
 
-@app.route('/portfolio/')
-def portfolio():
-	return render_template('portfolio.html')
+@app.route('/profile/')
+def profile():
+	return render_template('profile.html')
+
+@app.route('/search-list/')
+def search_list():
+    return render_template('search_list.html')
+
+@app.route('/search/')
+def search():
+    return render_template('search.html')
+
+@app.route('/settings/')
+def settings():
+    return render_template('settings.html')
+
+@app.route('/step1/')
+def step1():
+    return render_template('step1.html')
+
+@app.route('/step2/')
+def step2():
+    return render_template('step2.html')
