89ef94f4573e28284217b5b1d634fc3f00a49a5b
Samir Makhani <makhani@berkeley.edu>
2014-02-26 15:14:55 -0800

    how it works complete

--- app/static/js/momsitting.js
+++ app/static/js/momsitting.js
@@ -9,6 +9,19 @@ $(document).ready(function() {
     $(".general-nav").toggleClass("active");
   });
 
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+
   $( "#slider-range" ).slider({
     range: true,
     min: 0,
